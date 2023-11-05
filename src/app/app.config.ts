459f9640184ad8aff2bf4b72327a8261ed433354
kaeh <corentin.grosdemouge@gmail.com>
2023-11-05 15:06:14 +0100

    add biome

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,27 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from "@angular/core";
+import { provideRouter } from "@angular/router";
 
-import { routes } from './app.routes';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { routes } from "./app.routes";
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { getAuth, provideAuth } from "@angular/fire/auth";
+import { getFirestore, provideFirestore } from "@angular/fire/firestore";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"tabletop-character-sheet-63388","appId":"1:492259526847:web:6b46a10332d20761ff9e0a","storageBucket":"tabletop-character-sheet-63388.appspot.com","apiKey":"AIzaSyApJRQSrZXZDwoINmoTbMZGD0xX2hsGt2g","authDomain":"tabletop-character-sheet-63388.firebaseapp.com","messagingSenderId":"492259526847"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))]
+	providers: [
+		provideRouter(routes),
+		importProvidersFrom(
+			provideFirebaseApp(() =>
+				initializeApp({
+					projectId: "tabletop-character-sheet-63388",
+					appId: "1:492259526847:web:6b46a10332d20761ff9e0a",
+					storageBucket: "tabletop-character-sheet-63388.appspot.com",
+					apiKey: "AIzaSyApJRQSrZXZDwoINmoTbMZGD0xX2hsGt2g",
+					authDomain: "tabletop-character-sheet-63388.firebaseapp.com",
+					messagingSenderId: "492259526847",
+				}),
+			),
+		),
+		importProvidersFrom(provideAuth(() => getAuth())),
+		importProvidersFrom(provideFirestore(() => getFirestore())),
+	],
 };
