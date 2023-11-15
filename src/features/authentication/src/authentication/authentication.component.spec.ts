cf833e9f66defd0b561d60085cda0046bea188b8
Corentin GROSDEMOUGE <corentin.grosdemouge@gmail.com>
2023-11-15 10:27:38 +0100

    run formatter

--- src/features/authentication/src/authentication/authentication.component.spec.ts
+++ src/features/authentication/src/authentication/authentication.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { AuthenticationComponent } from './authentication.component';
+import { AuthenticationComponent } from "./authentication.component";
 
-describe('AuthenticationComponent', () => {
-  let component: AuthenticationComponent;
-  let fixture: ComponentFixture<AuthenticationComponent>;
+describe("AuthenticationComponent", () => {
+	let component: AuthenticationComponent;
+	let fixture: ComponentFixture<AuthenticationComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AuthenticationComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(AuthenticationComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [AuthenticationComponent],
+		}).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+		fixture = TestBed.createComponent(AuthenticationComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it("should create", () => {
+		expect(component).toBeTruthy();
+	});
 });
